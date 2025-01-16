4424d5a46cf4c2a5dbad13c1b44e020fdfa93fbc
Ivan <vanya1254@yandex.ru>
2025-01-16 16:45:10 +0300

    feat: add&fix animate logic

--- components/ProgressBar/index.js
+++ components/ProgressBar/index.js
@@ -3,6 +3,8 @@ export class ProgressBar {
     this.container = container;
     this.state = "Normal"; // 'Normal', 'Animated', 'Hidden'
     this.value = 0;
+    this.step = 1;
+    this.timer = null;
 
     this.init();
   }
@@ -10,25 +12,43 @@ export class ProgressBar {
   init() {
     this.container.innerHTML = `
     <section class="progressWrapper">
-        <div class="progressBar">
-            <h3 class="progressBar_title">Progress</h3>
-            <svg class="bar" width="100%" height="100%">
-                <circle class="defaultCircle" stroke="#f1f1f1"       stroke-width="10" cx="50%" cy="50%" r="28%"
+      <h3 class="progressTitle">Progress</h3>
+      <div class="progressBar">
+          <svg class="bar" width="100%" height="100%">
+              <circle class="defaultCircle" stroke="var(--grey-color)" stroke-width="10" cx="50%" cy="50%"  r="28%"
                 fill="transparent" />
-                <circle class="bar__circle" stroke="#1b40e2"    stroke-width="10" cx="50%" cy="50%" r="28%"
-                    fill="transparent" />
-            </svg>
-        </div>
-        <div class="controller">
-            <input id="progressInput" type="number" min="0" max="100"/>
-            <label>Value</label>
-        </div>
+              <circle class="bar__circle" stroke="#1b40e2"  stroke-width="10" cx="50%" cy="50%" r="28%"
+                fill="transparent">
+              </circle>
+          </svg>
+      </div>
+      <div class="controller">
+          <div class="controller__row">
+              <input class="progressInput" id="progressInput"   type="number" min="0" max="100" placeholder="100" />
+              <label for="progressInput">Value</label>
+          </div>
+          <div class="controller__row">
+              <label class="toggle" for="animate">
+                  <input type="checkbox" id="animate" />
+                  <span class="toggle_switch"></span>
+              </label>
+              <p>Animate</p>
+          </div>
+          <div class="controller__row">
+              <label class="toggle" for="hide">
+                  <input type="checkbox" id="hide" />
+                  <span class="toggle_switch"></span>
+              </label>
+              <p>Hide</p>
+          </div>
+      </div>
     </section>
     `;
 
     this.circle = this.container.querySelector(".bar__circle");
     this.radius = this.circle.r.baseVal.value;
     this.circumference = 2 * Math.PI * this.radius;
+    console.log(`Radius: ${this.radius}, Circumference: ${this.circumference}`);
 
     this.circle.style.strokeDasharray = `${this.circumference} ${this.circumference}`;
     this.circle.style.strokeDashoffset = `${this.circumference}`;
@@ -36,19 +56,58 @@ export class ProgressBar {
     //Input for progressBar
     this.input = this.container.querySelector("#progressInput");
     this.input.addEventListener("change", (e) => {
-      this.setValue(Number.parseInt(e.target.value));
+      const value = Number.parseInt(e.target.value)
+        ? Number.parseInt(e.target.value)
+        : 0;
+
+      this.setValue(value);
+    });
+
+    //Animate for progressBar
+    this.animateChekbox = this.container.querySelector("#animate");
+    this.animateChekbox.addEventListener("change", (e) => {
+      this.state = this.state === "Animated" ? "Normal" : "Animated";
+      e.target.checked = this.state === "Animated";
+
+      if (e.target.checked) {
+        this.startAnimate();
+      } else {
+        this.stopAnimate();
+      }
     });
   }
 
   setValue(value) {
-    if (value < 0 || value > 100) return;
+    if (value < 0) value = 0;
+    if (value > 100) value = 100;
 
     this.value = value;
 
     if (this.circumference) {
       const offset = this.circumference - (value / 100) * this.circumference;
-
+      console.log(
+        `Value: ${value}, Offset: ${offset}, Circumference: ${this.circumference}`
+      );
       this.circle.style.strokeDashoffset = offset;
     }
+  }
+
+  startAnimate() {
+    this.state = "Animated";
+
+    if (this.timer) return;
+
+    this.timer = setInterval(() => {
+      this.value = (this.value + this.step) % 101;
+
+      this.setValue(this.value);
+    }, 30);
+  }
+
+  stopAnimate() {
+    this.state = "Normal";
+
+    clearInterval(this.timer);
+    this.timer = null;
   }
 }
--- index.js
+++ index.js
@@ -3,5 +3,3 @@ import { ProgressBar } from "./components/ProgressBar/index.js";
 const root = document.getElementById("root");
 
 const progressBar = new ProgressBar(root);
-
-// progressBar.setValue(30);
