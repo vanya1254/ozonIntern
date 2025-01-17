3711d3eb1f8ef05a248100f1c2e730a3da070ab7
Ivan <vanya1254@yandex.ru>
2025-01-17 17:09:46 +0300

    Updates

--- components/ProgressBar/index.js
+++ components/ProgressBar/index.js
@@ -14,10 +14,10 @@ export class ProgressBar {
     <section class="progressWrapper">
       <h3 class="progressTitle">Progress</h3>
       <div class="progressBar">
-          <svg class="bar" width="100%" height="100%" aria-label="Progress Bar" viewBox="0 0 100 100">
-              <circle class="defaultCircle" stroke="var(--grey-color)" stroke-width="10" cx="50%" cy="50%"  r="22%"
+          <svg class="bar" width="100%" height="100%" aria-label="Progress Bar">
+              <circle class="defaultCircle" stroke="var(--grey-color)" stroke-width="10" cx="50" cy="50"  r="22"
                 fill="transparent" />
-              <circle class="bar__circle" stroke="var(--primaryBlue-color)"  stroke-width="10" cx="50%" cy="50%" r="22%"
+              <circle class="bar__circle" stroke="var(--primaryBlue-color)"  stroke-width="10" cx="50" cy="50" r="22"
                 fill="transparent">
               </circle>
           </svg>
